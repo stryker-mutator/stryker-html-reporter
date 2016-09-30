100aee1f90f2addc331f3b85cce05d7e595f0e66
Nico Jansen <jansennico@gmail.com>
2016-09-30 23:44:48 +0200

    fix(tslint): Add linting

--- test/helpers/chaiHelpers.ts
+++ test/helpers/chaiHelpers.ts
@@ -3,11 +3,11 @@ import * as chaiAsPromised from 'chai-as-promised';
 import * as sinonChai from 'sinon-chai';
 
 (<any>chai).Assertion.addMethod('equalData', function (expected: any) {
-  var actual = this._obj;
-  for(var i in expected){
-    this.assert(expected[i] === actual[i], 
-    `expected #{this} to at least contain the field "${i}" with value "${expected[i]}", but was "${actual[i]}"`,
-    `expected #{this} to not contain the field "${i}" with value "${expected[i]}", but it did.`)
+  let actual = this._obj;
+  for (let i in expected) {
+    this.assert(expected[i] === actual[i],
+      `expected #{this} to at least contain the field "${i}" with value "${expected[i]}", but was "${actual[i]}"`,
+      `expected #{this} to not contain the field "${i}" with value "${expected[i]}", but it did.`);
   }
 });
 
