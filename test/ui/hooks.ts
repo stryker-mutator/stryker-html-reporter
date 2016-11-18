cad443d968bdb25db5a6d554942282b628870f0f
Nico Jansen <jansennico@gmail.com>
2016-11-18 07:19:48 +0100

    test(hooks): Fix code analysis warning

--- test/ui/hooks.ts
+++ test/ui/hooks.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { MutantResult, SourceFile } from 'stryker-api/report';
 const exampleMutations = (require('../integration/exampleMutations.json') as MutantResult[]).map(mutantResult => {
   while (mutantResult.sourceFilePath.indexOf('/') !== -1) {
-    mutantResult.sourceFilePath = mutantResult.sourceFilePath.replace('/', path.sep)
+    mutantResult.sourceFilePath = mutantResult.sourceFilePath.replace('/', path.sep);
   }
   return mutantResult;
 });
